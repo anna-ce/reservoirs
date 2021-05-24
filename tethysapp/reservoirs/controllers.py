8e89270c902901b5ecbb78e18310940b84ffee0f
anna-ce <acecil2@byu.edu>
2021-05-24 11:38:11 -0600

    variables updates

--- tethysapp/reservoirs/controllers.py
+++ tethysapp/reservoirs/controllers.py
@@ -7,6 +7,7 @@ from django.shortcuts import render
 from tethys_sdk.gizmos import SelectInput, RangeSlider
 from tethys_sdk.permissions import login_required
 from tethys_sdk.workspaces import app_workspace
+
 import json
 
 
@@ -15,6 +16,12 @@ def home(request):
     """
     Controller for the app home page.
     """
+
+    url = "http://128.187.106.131/app/index.php/dr/services/cuahsi_1_1.asmx?WSDL"
+    water = pwml.WaterMLOperations(url=url)
+    sites = water.GetSites()
+    sites_js = json.dumps(sites)
+    print(sites_js)
 
     variables = SelectInput(
         display_text='Select a Reservoir',
@@ -33,12 +40,11 @@ def home(request):
 
 
 def GetSites(request):
-    print("Entering the GetSites function")
+
     return_object = {}
     url = "http://128.187.106.131/app/index.php/dr/services/cuahsi_1_1.asmx?WSDL"
     water = pwml.WaterMLOperations(url=url)
     sites = water.GetSites()
     return_object['siteInfo'] = sites
-    print(sites)
 
     return JsonResponse(return_object)
